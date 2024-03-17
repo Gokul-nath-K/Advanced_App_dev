8277b99bbe4dc8c7f918fc3ac696d005a138f736
Gokul-nath-K <gokulnathk003@gmail.com>
2024-03-17 16:12:49 +0530

    Pages added

---
+++ Frontend/src/Pages/user/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-screen">
+      <div className="bg-[url('https://ik.imagekit.io/gokul/PartyEvent/wedding-1.jpg')] h-full">
+        Home
+      </div>
+    </div>
+  );
+};
+
+export default Home;
